54af3a976292b8a10c0639fcda4e7d0ade07e406
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2024-08-23 23:47:29 -0400

    added highlight when hovering

--- src/client/features/tasks/World.jsx
+++ src/client/features/tasks/World.jsx
@@ -5,16 +5,21 @@ import Europe1 from "../pictures/Europe1.png"
 import Asia1 from "../pictures/Asia1.png"
 import Africa1 from "../pictures/africa1.png"
 import Austrilia1 from "../pictures/austrilia1.png"
+import './styles.css'
+
 //* This file controls all the contients, australia is also mispelled */
+
+
+
 function App() {
   return (
     <div>
-      <img src={NorthAmerica1} style={{ width: '800px', height: 'auto', position: 'fixed', filter:'hue-rotate(180deg)' }}/>
-      <img src={SouthAmerica1} style={{ width: '800px', position: 'fixed',  left: 35, top: 350, filter:  'hue-rotate(180deg)' }}/>
-      <img src={Europe1} style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65, filter:  'hue-rotate(180deg)' }}/>
-      <img src={Asia1} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, filter: 'hue-rotate(180deg)' }}/>
-      <img src={Africa1} style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500, filter: 'hue-rotate(180deg)' }}/>
-        <img src={Austrilia1} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, filter: 'hue-rotate(180deg)' }}/>
+      <img src={NorthAmerica1} className="continent" style={{ width: '800px', height: 'auto', position: 'fixed',  }}/>
+      <img src={SouthAmerica1} className="continent" style={{ width: '800px', position: 'fixed',  left: 35, top: 350,    }}/>
+      <img src={Europe1} className="continent" style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65,  }}/>
+      <img src={Asia1} className="continent" style={{ width: '950px', height: 'auto', position: 'fixed', right: 50, top: 100, }}/>
+      <img src={Africa1} className="continent" style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500,  }}/>
+        <img src={Austrilia1} className="continent" style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800,  }}/>
     </div>
   );
 }
