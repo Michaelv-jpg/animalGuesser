072d1f2e5e67c412a60641a2d1c9562d3bc5455a
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2024-08-24 04:08:15 -0400

    added background water and fixed alignments

--- src/client/features/tasks/World.jsx
+++ src/client/features/tasks/World.jsx
@@ -7,6 +7,7 @@ import Africa1 from "../pictures/africa1.png"
 import Austrilia1 from "../pictures/austrilia1.png"
 import './styles.css'
 
+
 //* This file controls all the contients, australia is also mispelled */
 
 
@@ -15,11 +16,11 @@ function App() {
   return (
     <div>
       <img src={NorthAmerica1} className="continent" style={{ width: '800px', height: 'auto', position: 'fixed',  }}/>
-      <img src={SouthAmerica1} className="continent" style={{ width: '800px', position: 'fixed',  left: 35, top: 350,    }}/>
-      <img src={Europe1} className="continent" style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65,  }}/>
+      <img src={SouthAmerica1} className="continent" style={{ width: '900px', position: 'fixed',  left: 10, top: 350,    }}/>
+      <img src={Europe1} className="continent" style={{ width: '880px', height: 'auto', position: 'fixed', right: 580, top: 65,  }}/>
       <img src={Asia1} className="continent" style={{ width: '950px', height: 'auto', position: 'fixed', right: 50, top: 100, }}/>
       <img src={Africa1} className="continent" style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500,  }}/>
-        <img src={Austrilia1} className="continent" style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800,  }}/>
+        <img src={Austrilia1} className="continent" style={{ width: '600px', height: 'auto', position: 'fixed', right: 200, top: 700,  }}/>
     </div>
   );
 }
