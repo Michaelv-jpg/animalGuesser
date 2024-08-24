1efe0285c9922006dad15a7c23d09bedb52e95c7
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2024-08-23 21:48:39 -0400

    fixed map

--- src/client/features/tasks/World.jsx
+++ src/client/features/tasks/World.jsx
@@ -9,12 +9,12 @@ import austrilia from "../pictures/austrilia.png"
 function App() {
   return (
     <div>
-      <img src={NorthAmerica} style={{ width: '800px', height: 'auto', position: 'fixed' }}/>
-      <img src={SouthAmerica} style={{ width: '800px', position: 'fixed',  left: 35, top: 350 }}/>
-      <img src={Europe} style={{ width: '650px', height: 'auto', position: 'fixed', right: 640, top: 65, }}/>
-      <img src={Asia} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, }}/>
-      <img src={africa} style={{ width: '600px', height: 'auto', position: 'fixed', right: 550, top: 500, }}/>
-        <img src={austrilia} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, }}/>
+      <img src={NorthAmerica} style={{ width: '800px', height: 'auto', position: 'fixed', filter:'hue-rotate(180deg)' }}/>
+      <img src={SouthAmerica} style={{ width: '800px', position: 'fixed',  left: 35, top: 350, filter:  'hue-rotate(180deg)' }}/>
+      <img src={Europe} style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65, filter:  'hue-rotate(180deg)' }}/>
+      <img src={Asia} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, filter: 'hue-rotate(180deg)' }}/>
+      <img src={africa} style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500, filter: 'hue-rotate(180deg)' }}/>
+        <img src={austrilia} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, filter: 'hue-rotate(180deg)' }}/>
     </div>
   );
 }
