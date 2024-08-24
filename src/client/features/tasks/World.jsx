2019b40056407db9854fcf46a1e7964cfb9e802e
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2024-08-23 21:10:06 -0400

    added all continents

--- src/client/features/tasks/World.jsx
+++ src/client/features/tasks/World.jsx
@@ -1,32 +1,22 @@
-import { useSelector } from "react-redux";
-import { selectToken } from "../auth/authSlice";
-import { useGetTasksQuery } from "./taskSlice";
 
-import "./Tasks.less";
+import NorthAmerica from "../pictures/NorthAmerica.png"
+import SouthAmerica from "../pictures/SouthAmerica.png"
+import Europe from "../pictures/Europe.png"
+import Asia from "../pictures/Asia.png"
+import africa from "../pictures/africa.png"
+import austrilia from "../pictures/austrilia.png"
 
-/** Main interface for user to interact with their tasks */
-export default function Tasks() {
-  const token = useSelector(selectToken);
-  const { data: tasks, isLoading } = useGetTasksQuery();
-
-  if (!token) {
-    return <p>You must be logged in to see your tasks.</p>;
-  }
-
+function App() {
   return (
-    <div className="tasks">
-      <h1>Tasks</h1>
-      <h2>Add New Task</h2>
-      <NewTask />
-      <h2>Your Tasks</h2>
-      {isLoading && <p>Loading tasks...</p>}
-      {tasks && (
-        <ul>
-          {tasks.map((task) => (
-            <Task key={task.id} task={task} />
-          ))}
-        </ul>
-      )}
+    <div>
+      <img src={NorthAmerica} style={{ width: '800px', height: 'auto', position: 'fixed' }}/>
+      <img src={SouthAmerica} style={{ width: '800px', position: 'fixed',  left: 35, top: 350 }}/>
+      <img src={Europe} style={{ width: '650px', height: 'auto', position: 'fixed', right: 640, top: 65, }}/>
+      <img src={Asia} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, }}/>
+      <img src={africa} style={{ width: '600px', height: 'auto', position: 'fixed', right: 550, top: 500, }}/>
+        <img src={austrilia} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, }}/>
     </div>
   );
 }
+
+export default App;
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import World from "./features/tasks/World"
 import "./index.less";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
-import AuthForm from "./features/auth/AuthForm";
-import Tasks from "./features/tasks/World.jsx";
-import Root from "./layout/Root.jsx";
+
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
     children: [
       { path: "/", element: <World /> },
     ],
