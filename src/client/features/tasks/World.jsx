6bf9eadb738d8281e84455483f0633704b43a157
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2024-08-23 23:17:27 -0400

    all colors matched up

--- src/client/features/tasks/World.jsx
+++ src/client/features/tasks/World.jsx
@@ -1,20 +1,20 @@
 
-import NorthAmerica from "../pictures/NorthAmerica.png"
-import SouthAmerica from "../pictures/SouthAmerica.png"
-import Europe from "../pictures/Europe.png"
-import Asia from "../pictures/Asia.png"
-import africa from "../pictures/africa.png"
-import austrilia from "../pictures/austrilia.png"
-
+import NorthAmerica1 from "../pictures/NorthAmerica1.png"
+import SouthAmerica1 from "../pictures/SouthAmerica1.png"
+import Europe1 from "../pictures/Europe1.png"
+import Asia1 from "../pictures/Asia1.png"
+import Africa1 from "../pictures/africa1.png"
+import Austrilia1 from "../pictures/austrilia1.png"
+//* This file controls all the contients, australia is also mispelled */
 function App() {
   return (
     <div>
-      <img src={NorthAmerica} style={{ width: '800px', height: 'auto', position: 'fixed', filter:'hue-rotate(180deg)' }}/>
-      <img src={SouthAmerica} style={{ width: '800px', position: 'fixed',  left: 35, top: 350, filter:  'hue-rotate(180deg)' }}/>
-      <img src={Europe} style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65, filter:  'hue-rotate(180deg)' }}/>
-      <img src={Asia} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, filter: 'hue-rotate(180deg)' }}/>
-      <img src={africa} style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500, filter: 'hue-rotate(180deg)' }}/>
-        <img src={austrilia} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, filter: 'hue-rotate(180deg)' }}/>
+      <img src={NorthAmerica1} style={{ width: '800px', height: 'auto', position: 'fixed', filter:'hue-rotate(180deg)' }}/>
+      <img src={SouthAmerica1} style={{ width: '800px', position: 'fixed',  left: 35, top: 350, filter:  'hue-rotate(180deg)' }}/>
+      <img src={Europe1} style={{ width: '850px', height: 'auto', position: 'fixed', right: 580, top: 65, filter:  'hue-rotate(180deg)' }}/>
+      <img src={Asia1} style={{ width: '800px', height: 'auto', position: 'fixed', right: 50, top: 100, filter: 'hue-rotate(180deg)' }}/>
+      <img src={Africa1} style={{ width: '850px', height: 'auto', position: 'fixed', right: 500, top: 500, filter: 'hue-rotate(180deg)' }}/>
+        <img src={Austrilia1} style={{ width: '300px', height: 'auto', position: 'fixed', right: 200, top: 800, filter: 'hue-rotate(180deg)' }}/>
     </div>
   );
 }
